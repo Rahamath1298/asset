1800fd7d419417b75764413828b0ca9c77792c19
Rahamath <76873838+Rahamath1298@users.noreply.github.com>
2021-08-02 16:03:50 +0530

    commit

---
+++ header.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Assetwala.com</title>
+<link rel="shortcut icon" type="image/png" href="images/logo1.png" />
+<meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+ 	<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.css" />
+  <link rel="stylesheet" href="assets/style.css"/>
+  <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+	<script src="assets/bootstrap/js/bootstrap.js"></script>
+  <script src="assets/script.js"></script>
+<link rel="manifest" href="manifest.json">
+    <script>
+        //if browser support service worker
+        if('serviceWorker' in navigator) {
+          navigator.serviceWorker.register('sw.js');
+        };
+      </script>
+
+
+<!-- Owl stylesheet -->
+<link rel="stylesheet" href="assets/owl-carousel/owl.carousel.css">
+<link rel="stylesheet" href="assets/owl-carousel/owl.theme.css">
+<script src="assets/owl-carousel/owl.carousel.js"></script>
+<!-- Owl stylesheet -->
+
+
+<!-- slitslider -->
+    <link rel="stylesheet" type="text/css" href="assets/slitslider/css/style.css" />
+    <link rel="stylesheet" type="text/css" href="assets/slitslider/css/custom.css" />
+    <script type="text/javascript" src="assets/slitslider/js/modernizr.custom.79639.js"></script>
+    <script type="text/javascript" src="assets/slitslider/js/jquery.ba-cond.min.js"></script>
+    <script type="text/javascript" src="assets/slitslider/js/jquery.slitslider.js"></script>
+<!-- slitslider -->
+<style type="text/css">
+  
+
+        .GFG { 
+            background-color: white; 
+            border: 2px solid black; 
+            color: green; 
+            padding: 5px 10px; 
+            text-align: center; 
+            display: inline-block; 
+            font-size: 20px; 
+            margin: 10px 30px; 
+            cursor: pointer; 
+            text-decoration:none; 
+        } 
+    
+    
+</style>
+</head>
+
+<body>
+
+
+<!-- Header Starts -->
+<div class="navbar-wrapper">
+
+        <div class="navbar-inverse" role="navigation">
+          <div class="container">
+            <div class="navbar-header">
+
+
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+
+            </div>
+
+
+            <!-- Nav Starts -->
+            <div class="navbar-collapse  collapse">
+              <ul class="nav navbar-nav navbar-right">
+               <li class="active"><a href="index.php">Home</a></li>
+                <li><a href="about.php">MAP</a></li>
+                <li><a href="agents.php">NRI SECTION</a></li>         
+                <li><a href="blog.php">SERVICES</a></li>
+                <li><a href="contact.php">FEATURED</a></li>
+                <li><a href="about.php">ABOUT US</a></li>
+                <li><a href="register.php">REGISTER/LOGIN</a></li>
+              </ul>
+            </div>
+            <!-- #Nav Ends -->
+
+          </div>
+        </div>
+
+    </div>
+<!-- #Header Starts -->
+
+
+
+
+
+<div class="container">
+
+<!-- Header Starts -->
+<div class="header">
+<a href="index.php"><img src="images/logo1.png" style="height:75px; width: 230px; " alt="Realestate"></a>
+
+              <ul class="pull-right">
+                <li><a href="buysalerent.php">Sale</a></li>
+                <li><a href="buysalerent.php">Rent</a></li>         
+                <li><a href="buysalerent.php">Sale & Rent</a></li>
+              </ul>
+</div>
+<!-- #Header Starts -->
+</div>
---
+++ login.php
@@ -0,0 +1,34 @@
+<?php include'header.php';?>
+<!-- banner -->
+<div class="inside-banner">
+  <div class="container"> 
+    <span class="pull-right"><a href="index.php">Home</a> / Login</span>
+    <h2>Login</h2>
+</div>
+</div>
+<!-- banner -->
+
+
+<div class="container">
+<div class="spacer">
+<div class="row register">
+  <div class="col-lg-6 col-lg-offset-3 col-sm-6 col-sm-offset-3 col-xs-12 ">
+
+
+                <input type="text" class="form-control" placeholder="Full Name" name="form_name">
+                
+                <input type="password" class="form-control" placeholder="Password" name="form_phone">
+
+                <button type="submit" class="btn btn-success" name="Submit">Login</button>
+                <a href="register.php" class="GFG">Register</a>
+          
+
+
+                
+        </div>
+  
+</div>
+</div>
+</div>
+
+<?php include'footer.php';?>
